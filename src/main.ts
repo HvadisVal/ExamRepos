dd6e38e93188d2a65f90e4f1c01a14d7f5e34c8c
HvadisVal <114469812+HvadisVal@users.noreply.github.com>
2023-11-08 14:35:57 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,43 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+function add(x: number, y: number): number {
+    return x + y;
+}
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite loogo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+function subtract(x: number, y: number): number {
+    return x - y;
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function multiply(x: number, y: number): number {
+    return x * y;
+}
+
+function divide(x: number, y: number): number {
+    if (y !== 0) {
+        return x / y;
+    } else {
+        throw new Error("Division by zero is not allowed");
+    }
+}
+
+function appendToResult(value: string): void {
+    const resultElement = document.getElementById('result') as HTMLInputElement;
+    resultElement.value += value;
+}
+
+function clearResult(): void {
+    const resultElement = document.getElementById('result') as HTMLInputElement;
+    resultElement.value = '';
+}
+
+function calculateResult(): void {
+    try {
+        const resultElement = document.getElementById('result') as HTMLInputElement;
+        const result = eval(resultElement.value);
+        resultElement.value = result.toString();
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(error.message);
+        } else {
+            console.error("An unknown error occurred");
+        }
+    }
+}
